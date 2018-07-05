55be0baac0b061935428f497347be30fe3ce8004
Lucas Wang <lucas211w@gmail.com>
2018-07-05 19:02:43 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -2,7 +2,7 @@ def turn(board)
   puts "Please enter 1-9:"
   user_input = gets.chomp
   index = input_to_index(user_input)
-  while !valid_move(board,index)
+  while !valid_move?(board,index)
     turn(board)
   end
 end
